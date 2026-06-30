efbedf22f77af2f8486d21c0da6f2a75ef3c800b
BryanJazo-d <bryan011215@gmail.com>
2026-06-30 03:40:10 +0000

    correccion2

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-function abrirRecuerdos(){
+function abrirRecuerdos() {
 
-const fecha=document.getElementById("fecha").value.trim();
+    const fecha = document.getElementById("fecha").value.trim();
 
-if(fecha==="19/09/2024" || fecha==="19-09-2024"){
+    if (fecha === "19/09/2024" || fecha === "19-09-2024") {
 
-window.location.href="galeria.html";
+        window.location.href = "galeria.html";
 
-}else{
+    } else {
 
-alert("Esa fecha no es correcta ❤️");
+        alert("Esa fecha no es correcta 💗");
+
+    }
 
 }
+
+function toggleMusica() {
+
+    const audio = document.getElementById("musica");
+    const boton = document.getElementById("musicaBtn");
+
+    if (audio.paused) {
+
+        audio.play();
+        boton.innerHTML = "⏸ Pausar nuestra canción";
+
+    } else {
+
+        audio.pause();
+        boton.innerHTML = "🎵 Reproducir nuestra canción";
+
+    }
 
 }
